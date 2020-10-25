6204df43b1dd935eb66176c3f3972c0dca5f9165
Galen Guyer <galen@galenguyer.com>
2020-10-25 13:43:41 -0400

    add basic is open route

--- ritdiningapi/__init__.py
+++ ritdiningapi/__init__.py
@@ -1,9 +1,13 @@
 """ A small flask Hello World """
 
+from dateutil import parser, tz
+from datetime import *
 import os
 import subprocess
+import requests
 
 from flask import Flask, jsonify
+from bs4 import BeautifulSoup
 
 APP = Flask(__name__)
 
@@ -15,6 +19,25 @@ else:
 
 APP.secret_key = APP.config['SECRET_KEY']
 
-@APP.route('/')
+@APP.route('/api/v1/open')
 def _index():
-    return jsonify(status=200, response="OK")
+    page = requests.get('https://www.rit.edu/fa/diningservices/places-to-eat/hours')
+    parsed_page = BeautifulSoup(page.text, 'html.parser')
+    hours_divs = parsed_page.findAll('div', class_='view-places-to-eat')[0].find('div').findAll('div', recursive=False)
+    hours = {}
+    lastPlace = ''
+    for div in hours_divs:
+        if 'hours-title' in div.attrs['class']:
+            lastPlace = div.text.strip()
+            hours[lastPlace] = {}
+        else:
+            if hours[lastPlace].get('open') is not True and 'Closed' in div.text.strip():
+                hours[lastPlace]['open'] = False
+            else:
+                text = div.text.strip()
+                open = parser.parse(text.split(' ')[-3], ignoretz=True).astimezone(tz.gettz("America/New_York"))
+                close = parser.parse(text.split(' ')[-1], ignoretz=True).astimezone(tz.gettz("America/New_York"))
+                now = datetime.now(timezone.utc)
+                if open < now < close:
+                    hours[lastPlace]['open'] = True
+    return jsonify(hours)
